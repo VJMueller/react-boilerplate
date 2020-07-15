5c8ccb40b0049f9c40060a1916dbf00b1fd89c5a
vmueller <vmueller@vmuellers-MBP.fritz.box>
2020-07-15 14:51:13 +0200

    Added Webpack, basic config

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-const greet = (name) => console.log(`Hello, ${name}`);
+import sum from './sum';
 
+console.log(sum(2, 4)) // Output: 6
+const greet = (name) => console.log(`Hello, ${name}`);
 greet("Jon Snow");
